726970a3060f8d259697b97f02ed1b0e7afc9e26
umarmansoor001 <i190594@nu.edu.pk>
2022-09-22 20:45:15 +0500

    add person function added

--- task_1.go
+++ task_1.go
@@ -9,10 +9,18 @@ type Person struct {
 	salary int
 }
 
+func add_person(name string, age int, job string, salary int) Person {
+	var p Person
+	p.name = name
+	p.age = age
+	p.job = job
+	p.salary = salary
+	return p
+}
+
 func Display_person(p Person) {
 	fmt.Println("Name: ", p.name)
 	fmt.Println("Age: ", p.age)
 	fmt.Println("Job: ", p.job)
 	fmt.Println("Salary: ", p.salary)
 }
-
